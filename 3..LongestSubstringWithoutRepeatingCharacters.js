bfd621e7f636e6cd58f006cadd7f0a6210fb42a9
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 00:52:24 +0700

    Add 3..LongestSubstringWithoutRepeatingCharacters.js file

---
+++ 3..LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,19 @@
+function lengthOfLongestSubstring(s) {
+    let set = new Set();
+    let maxLength = 0;
+    let left = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+
+// Example
+console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
