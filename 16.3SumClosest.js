8d3e9f353dbd4f6d72e9b137133115587b19fa6a
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 01:36:36 +0700

    Add 16.3SumClosest.js file

---
+++ 16.3SumClosest.js
@@ -0,0 +1,24 @@
+function threeSumClosest(nums, target) {
+    nums.sort((a, b) => a - b);
+    let closest = Infinity;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1, right = nums.length - 1;
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                closest = sum;
+            }
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return sum;
+            }
+        }
+    }
+    return closest;
+}
+
+// Example
+console.log(threeSumClosest([-1, 2, 1, -4], 1)); // Output: 2
