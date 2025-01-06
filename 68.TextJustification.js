cd93fe9c46b2e7b32b50e237d40d2b2b4a94d7cd
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-07 00:16:14 +0700

    Add 68.TextJustification.js file

---
+++ 68.TextJustification.js
@@ -0,0 +1,25 @@
+function fullJustify(words, maxWidth) {
+    let res = [];
+    let cur = [];
+    let curLen = 0;
+
+    for (let word of words) {
+        if (curLen + word.length + cur.length > maxWidth) {
+            let spaces = maxWidth - curLen;
+            for (let i = 0; i < spaces; i++) {
+                cur[i % (cur.length - 1 || 1)] += ' ';
+            }
+            res.push(cur.join(''));
+            cur = [];
+            curLen = 0;
+        }
+        cur.push(word);
+        curLen += word.length;
+    }
+
+    res.push(cur.join(' ') + ' '.repeat(maxWidth - curLen - cur.length + 1));
+    return res;
+}
+
+// Example
+console.log(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16));
