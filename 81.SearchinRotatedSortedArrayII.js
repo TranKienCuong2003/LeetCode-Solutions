fbc190fd3e4faf2624cb6aff181fcd50331ccd72
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-09 01:48:39 +0700

    Add 81.SearchinRotatedSortedArrayII.js file

---
+++ 81.SearchinRotatedSortedArrayII.js
@@ -0,0 +1,38 @@
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return true;
+        }
+
+        if (nums[left] === nums[mid] && nums[mid] === nums[right]) {
+            left++;
+            right--;
+        }
+        else if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return false;
+};
+
+// Example
+console.log(search([2, 5, 6, 0, 0, 1, 2], 0));
+
+console.log(search([2, 5, 6, 0, 0, 1, 2], 3));
